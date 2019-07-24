b5c5e5e0337c7cb01698bba3d4151cda9937dc91
haynar216 <1500012913@pku.edu.cn>
2019-07-25 03:55:08 +0800

    add test_token.js and modify test_account、test_blockchain、test_tx

--- test/test_blockchain.js
+++ test/test_blockchain.js
@@ -24,6 +24,7 @@ const token_id = "TWtLLjtURLq5ybDHgNnFoQKvaBmUqbxMQgewzs8Ru";
 
 const contract_id = "CEzgXYsSJ8YerN31DqC42q4in44KgijgJFo";
 
+const transaction_id = '3pWxPKy3QkChWHd6qnkTbVH6NCEr3ezisE8WrYMfNW2i';
 /*================ Change end ==================*/
 async function testBalance(chain, address) {
     let result = await chain.getBalance(address);
@@ -99,57 +100,24 @@ async function testContractContent(chain, contract_id) {
 // Test Blockchain
 const chain = new Blockchain(host_ip, network_byte);
 
-//test Balance
-const test_balance_result =
-    { address: 'AUAztxsft2v6rmjRRb72nLea6BNyRHHWpUR',
-    confirmations: 0,
-    balance: 58633509999997 };
-
-describe('testBalance', function () {
+//test balance
+describe('test get balance by address', function () {
     it('get the balance', async () =>{
         let result = await testBalance(chain, address);
-        expect(result).to.deep.equal(test_balance_result);
+        expect(result['address']).to.be.equal(address);
     })
 });
 
 
 //test balanceDetail
-const test_balance_detail_result = {
-    address: 'AUAztxsft2v6rmjRRb72nLea6BNyRHHWpUR',
-    regular: 58633509999997,
-    mintingAverage: 58194272087287,
-    available: 58406609999997,
-    effective: 58407409999997,
-    height: 5920795 };
-
 describe('testBalanceDetail', function () {
     it('get address:', async () =>{
         let result = await testBalanceDetail(chain, address);
-        expect(result['address']).to.deep.equal(test_balance_detail_result['address']);
-        expect(result['address']).to.be.a('string');
-    });
-    it('get regular:', async () =>{
-        let result = await testBalanceDetail(chain, address);
-        expect(result['regular']).to.deep.equal(test_balance_detail_result['regular']);
-        expect(result['regular']).to.be.a('number');
-    });
-    it('get mintingAverage:', async () =>{
-        let result = await testBalanceDetail(chain, address);
-        expect(result['mintingAverage']).to.be.a('number');
+        expect(result['address']).to.be.equal(address);
     });
     it('get available:', async () =>{
         let result = await testBalanceDetail(chain, address);
-        expect(result['available']).to.deep.equal(test_balance_detail_result['available']);
         expect(result['available']).to.be.a('number');
-    });
-    it('get effective:', async () =>{
-        let result = await testBalanceDetail(chain, address);
-        expect(result['effective']).to.deep.equal(test_balance_detail_result['effective']);
-        expect(result['effective']).to.be.a('number');
-    });
-    it('get height:', async () =>{
-        let result = await testBalanceDetail(chain, address);
-        expect(result['height']).to.be.a('number');
     });
 });
 
@@ -161,25 +129,7 @@ describe('testTxHistory', function () {
     })
 });
 
-
 //test TxById
-const test_txById_result = {
-    type: 2,
-    id: 'DfHnAowHFRNhYUGR3SbeATuBxYQCXCXihBDkcp6UPWeX',
-    fee: 10000000,
-    timestamp: 1562923197699000000,
-    proofs:
-    [ { proofType: 'Curve25519',
-        publicKey: 'DWKUGdT1HL4a3zrhoeRJd2zfKRPxmknRotdGVFrViK7o',
-        signature: 'X4j5rMWaWtwvBTqqr9JksaEkF39iXzmRd8m3iu2sz91Zzjvf7UpusfyuQ3zbGe72epvVeCjXs7h2ASta3WwFGMr' } ],
-        recipient: 'AUEMZKy23xvWixKySNDg448dXxwc4GEZCC3',
-    feeScale: 100,
-    amount: 10000000000,
-    attachment: 'r2pimgne',
-    status: 'Success',
-    feeCharged: 10000000,
-    height: 5581261 };
-
 describe('testTxById', function () {
     it('get the TxById', async () =>{
         let result = await testTxById(chain, tx_id);
@@ -187,14 +137,11 @@ describe('testTxById', function () {
     });
     it('status is success', async () =>{
         let result = await testTxById(chain, tx_id);
-        expect(result['status']).to.be.equal(test_txById_result['status']);
+        expect(result['status']).to.be.equal('Success');
     });
 });
 
 //test unconfirmedTxById
-const test_unconfirmedTxById_result = {
-    status: 'error', details: 'Transaction is not in UTX' };
-
 describe('testUnconfirmedTxById', function () {
     it('get the unconfirmedTxById', async () =>{
         let result = await testUnconfirmedTxById(chain, address);
@@ -202,7 +149,7 @@ describe('testUnconfirmedTxById', function () {
     });
     it('status is error', async () =>{
         let result = await testUnconfirmedTxById(chain, address);
-        expect(result['status']).to.be.equal(test_unconfirmedTxById_result['status']);
+        expect(result['status']).to.be.equal('error');
     });
 });
 
@@ -223,41 +170,14 @@ describe('testLastBlock', function () {
 });
 
 //test BlockByHeight
-const test_blockByHeight = {
-    version: 1,
-    timestamp: 1544760641000697300,
-    reference: '2U3ejvTHjQ6tkgfdzxmd4vnhnhCGcqhuaJEPg7EWy3cRoMKLyxhfgGHUMWgfk5Q2v3T4oFp755PqmziDpNQ7J8nw',
-    SPOSConsensus: { mintTime: 1544760641000000000, mintBalance: 26906969405 },
-    resourcePricingData:
-        { computation: 0,
-            storage: 0,
-            memory: 0,
-            randomIO: 0,
-            sequentialIO: 0 },
-    TransactionMerkleRoot: '9JwRwyzXZX6ympZty45jb3MH4W5c9DaSUqNPrQA8YVqm',
-    transactions:
-        [ { type: 5,
-            id: '7Cb3np9QS3PWhxa35qjC6fWrynacvC3BxsVSqx46VMp2',
-            recipient: 'AU31Me4Lvi1Th5uK9z4JXe3fq1BfkcNTpis',
-            timestamp: 1544760641000697300,
-            amount: 900000000,
-            currentBlockHeight: 56,
-            status: 'Success',
-            feeCharged: 0 } ],
-    generator: 'AU31Me4Lvi1Th5uK9z4JXe3fq1BfkcNTpis',
-    signature: '2JsK7eezuiKpmVZMm8RLZjNZ4UUEWiFPUYCPnYCxXRcGZTGp5hUiBTMEqMMYzhvSa3G693mrJTEoNEhdP8bK8M3H',
-    fee: 0,
-    blocksize: 330,
-    height: 56,
-    'transaction count': 1 };
 describe('testBlockByHeight', function () {
     it('get the BlockByHeight', async () =>{
         let result = await testBlockByHeight(chain, height);
         expect(result).to.not.be.empty;
     });
-    it('get the signature', async () =>{
+    it('correct height', async () =>{
         let result = await testBlockByHeight(chain, height);
-        expect(result['signature']).to.be.equal(test_blockByHeight['signature']);
+        expect(result['height']).to.be.equal(height);
     });
 });
 
@@ -280,92 +200,46 @@ describe('testAllSlotsInfo', function () {
 
 
 //test TokenInfo
-const test_tokenInfo_result = {
-    tokenId: 'TWtLLjtURLq5ybDHgNnFoQKvaBmUqbxMQgewzs8Ru',
-    contractId: 'CEzgXYsSJ8YerN31DqC42q4in44KgijgJFo',
-    max: 1000000000000000,
-    total: 1000000000000,
-    unity: 100000000,
-    description: '13gQZ2GTk' };
-
 describe('testTokenInfo', function () {
     it('get the TokenInfo', async () =>{
         let result = await testTokenInfo(chain, token_id);
         expect(result).to.not.be.empty;
     });
-    it('get the contractId', async () =>{
+    it('get the tokenId', async () =>{
         let result = await testTokenInfo(chain, token_id);
-        expect(result['contractId']).to.be.equal(test_tokenInfo_result['contractId']);
+        expect(result['tokenId']).to.be.equal(token_id);
     });
 });
 
 //test TokenBalance
-const test_tokenBalance= {
-    address: 'AUAztxsft2v6rmjRRb72nLea6BNyRHHWpUR',
-    tokenId: 'TWtLLjtURLq5ybDHgNnFoQKvaBmUqbxMQgewzs8Ru',
-    balance: 1000000000000,
-    unity: 100000000 };
 describe('testTokenBalance', function () {
     it('get the TokenBalance', async () =>{
         let result = await testTokenBalance(chain, address, token_id);
         expect(result).to.not.be.empty;
     });
-    it('get the balance', async () =>{
+    it('get the address', async () =>{
         let result = await testTokenBalance(chain, address, token_id);
-        expect(result['balance']).to.be.equal(test_tokenBalance['balance']);
+        expect(result['address']).to.be.equal(address);
+    });
+    it('get the tokenId', async () =>{
+        let result = await testTokenBalance(chain, address, token_id);
+        expect(result['tokenId']).to.be.equal(token_id);
     });
 });
 
 //test ContractInfo
-const test_contractInfo = {
-    contractId: 'CEzgXYsSJ8YerN31DqC42q4in44KgijgJFo',
-    transactionId: '3pWxPKy3QkChWHd6qnkTbVH6NCEr3ezisE8WrYMfNW2i',
-    type: 'TokenContractWithSplit',
-    info:
-        [ { data: 'AUAztxsft2v6rmjRRb72nLea6BNyRHHWpUR',
-            type: 'Address',
-            name: 'issuer' },
-            { data: 'AUAztxsft2v6rmjRRb72nLea6BNyRHHWpUR',
-                type: 'Address',
-                name: 'maker' } ],
-    height: 5581169 };
 describe('testContractInfo', function () {
     it('get the ContractInfo', async () =>{
         let result = await testContractInfo(chain, contract_id);
         expect(result).to.not.be.empty;
     });
-    it('get the transactionId', async () =>{
+    it('get the contractId', async () =>{
         let result = await testContractInfo(chain, contract_id);
-        expect(result['transactionId']).to.be.equal(test_contractInfo['transactionId']);
+        expect(result['contractId']).to.be.equal(contract_id);
     });
 });
 
 //test ContractContent
-const test_contractContent_result = {
-    transactionId: '3pWxPKy3QkChWHd6qnkTbVH6NCEr3ezisE8WrYMfNW2i',
-    languageCode: 'vdds',
-    languageVersion: 1,
-    triggers: [ '111111CktRzdj615GhYiN5qtRjzxwE8jypDwGbQV9iXn8NK3o' ],
-    descriptors:
-        [ '1111112EP7Gb96dj5VLAcfpEDiaTeapNEfczB',
-            '1bbn7XmN81WxPGPpdCtU38xMGvgaUbnrASoSQf',
-            '12CCZE4Xi2itkMk8zE87pKF1mN8U311U2Bq99jV',
-            '1QWywjfJS2CDokeThm2PVEKPzag9nouQ72jYj',
-            '1N9hmWGg5UN8zHMrtrxMWFND6pUMLhAGg',
-            '131h1vYVUznedmBCAvcPqzW6Ewx5xvXF4fB',
-            '13R2cuenmhy573wnHtSch5h2jSJQ3hS6h1B',
-            '13pNDtm64QxxY3tNu8tVZiwUAPB8TP9cVs7',
-            '1VXrvftSE5dDWxAQwUHSpM3jdx2FR',
-            '1Z6ifdCDh5xNbucPnydpgg2nbfS3R',
-            '1Cyp7C43k4foxpiwcrr33L3mCEKxLsoe',
-            '13zAHzf98UyzPAVrFiE8sQLcUX6EcSK' ],
-    stateVariables: [ '13', '5T' ],
-    textual:
-        { triggers: '124VnyFU9tQUn4Z19KBbV8aAQF4aCgWrQWrLL1yK5RpWY2sU74P8GU6wJ6dwyuFHP3Xt5Kmpm',
-            descriptors: '1RypGiL5eNbDESxn2SVM8HrLF6udvXV6YmwvFsp4fLJfRcr7nQuVFMvXn6KmWJeq8c53tdrutZcsQA2zyHb8Wj1tQUjGmitP6kLzcnpQXEq7AUZpMT6j7LCrhJvs3oLCCr7SSpz3h4iJJJg9WuL7Acbsw1x2AK4tRSZWXyrnLgqWhgqbTdfmxFGHjD58XrScBibJ9AUwEWCAeAna3NFofSZaSDxFJAK2adrrHhJdktQCQobMJMmC164HtJKF569naoMREkncYedQwXWk4uyPzGTUKsfXFwLaR77wv8gtNEjqwvGtpdFJELyJ3RC2F7exhqiiVxTaoGrAanuv1bianVbKqPAygPaGrhA1H3JmQWksNhg6q7dtPvBuqWDqDs4DkhV35JhNFeiER18o49pxX8zR1n1jvis6QrU2cD1Cn3yXwSZaW8TXWMKZ7ULRo1UcJykQvQCLq3EBVfzf6iULhuRagTnJ3Sq4tFSxgnNPhATLDreQpEe1BA3SfRWKRskLFjXV5aMeYxgFLfqYEFJ37BaRVyFZDSUgrKLMnNzrZZG2P81t7MhT6GpDApLZkNtjdGRMQGFsRN2azGruQReFnXeB3mScaxgfhGxcu9B',
-            stateVariables: '1FKqt4aNuTwK15xVSfjkwT' },
-    height: 5581169 };
-
 describe('testContractContent', function () {
     it('get the ContractContent', async () =>{
         let result = await testContractContent(chain, contract_id);
@@ -373,7 +247,7 @@ describe('testContractContent', function () {
     });
     it('get the transactionId', async () =>{
         let result = await testContractContent(chain, contract_id);
-        expect(result['transactionId']).to.be.equal(test_contractContent_result['transactionId']);
+        expect(result['transactionId']).to.be.equal(transaction_id);
     });
 });
 
--- test/test_tx.js
+++ test/test_tx.js
@@ -1,53 +1,93 @@
-import "babel-polyfill";
+// import "babel-polyfill";
 const Transaction = require('../libs/transaction');
 const Account = require('../libs/account');
 var constants = require("../libs/constants");
-
+var expect = require("chai").expect;
 const network_byte = constants.TESTNET_BYTE;
 const host_ip = 'http://test.v.systems:9922';
+var test_lease_id = '';
+var cancel_lease_result = '';
 
 /*======= Change the below before run ==========*/
 const seed = "alter glare wealth alert about inmate wild foster nothing track brown chief primary acquire energy";
 const recipient = "AUEMZKy23xvWixKySNDg448dXxwc4GEZCC3";
+const sender_public_key = "DWKUGdT1HL4a3zrhoeRJd2zfKRPxmknRotdGVFrViK7o";
 const attachment = "hello world";
 const nonce = 0;
-const amount = 1000;
-const test_lease_id = "HYSi8CXcwXkvTEwa5eSY7ksjyHnMyaMMcGqdPyYSTY5a"; // only use once
+const amount = 1;
 /*================ Change end ==================*/
 
+async function sendPaymentTx(acc, tx) {
+    let node = host_ip + '/vsys/broadcast/payment';
+    const result = await acc.sendTransactionTx(node, tx);
+    return result;
+}
 
+async function sendLeasingTx(acc, tx) {
+    let node = host_ip + '/leasing/broadcast/lease';
+    const result = await acc.sendTransactionTx(node, tx);
+    return result;
+}
 
-//paymentTx test, (send amount VSYS)
-function testPaymentTx(amount) {
-    console.log("test PaymentTx begin:");
+async function sendCancelLeasingTx(acc, tx) {
+    let node = host_ip + '/leasing/broadcast/cancel';
+    cancel_lease_result = await acc.sendTransactionTx(node, tx);
+}
 
+//test payment tx (send amount VSYS) and buildTransactionId
+describe('test payment tx', function () {
     let acc =  new Account(network_byte);
     acc.buildFromSeed(seed, nonce);
     let public_key = acc.getPublicKey();
     let tra = new Transaction(network_byte);
 
+    let result = '';
     let timestamp = Date.now() * 1e6;
     tra.buildPaymentTx(public_key, recipient, amount, attachment, timestamp);
 
     let bytes = tra.toBytes();
+    let transaction_id = tra.buildTransactionId(bytes);
     let signature = acc.getSignature(bytes);
-    let sendTx = tra.toJsonForSendingTx(signature);
-    sendPaymentTx(acc, sendTx);
+    let send_tx = tra.toJsonForSendingTx(signature);
 
-    let coldSig = tra.toJsonForColdSignature();
-    console.log("json for cold signature:");
-    console.log(coldSig);
+    let cold_tx = tra.toJsonForColdSignature()
+    it('get send tx', function () {
+        expect(send_tx).to.not.be.empty;
+    });
+    it('get cold tx', function () {
+        expect(cold_tx).to.not.be.empty;
+    });
+    it('get sender publicKey in cold tx', function () {
+        expect(cold_tx['senderPublicKey']).to.be.equal(sender_public_key);
+    });
+    it('get recipient in cold tx', function () {
+        expect(cold_tx['recipient']).to.be.equal(recipient);
+    });
+    it('get correct transactionType in cold tx', function () {
+        expect(cold_tx['transactionType']).to.be.equal(constants.PAYMENT_TX);
+    });
+    it('get correct opc in cold tx', function () {
+        expect(cold_tx['opc']).to.be.equal(constants.OPC_TRANSACTION);
+    });
+    it('get protocol in cold tx', function () {
+        expect(cold_tx['protocol']).to.be.equal(constants.PROTOCOL);
+    });
+    it('get sendPayment tx result', async ()=>{
+        result = await sendPaymentTx(acc, send_tx);
+        expect(result).to.not.be.empty;
+        expect(result['recipient']).to.be.equal(recipient);
+        expect(result['amount']).to.be.equal(send_tx['amount']);
+        expect(result['attachment']).to.be.equal(send_tx['attachment']);
+        expect(result['type']).to.be.equal(constants.PAYMENT_TX);
+    });
+    it('build correct transactionId', function () {
+        expect(result['id']).to.be.equal(transaction_id);
+    });
+});
 
-    console.log("test PaymentTx end:")
-}
 
-
-
-
-//LeasingTx test, (lease amount VSYS)
-function testLeasingTx(amount) {
-    console.log("test LeasingTx begin:");
-
+//test leasing tx (lease amount VSYS)
+describe('test leasing tx', function () {
     let acc =  new Account(network_byte);
     acc.buildFromSeed(seed, nonce);
     let public_key = acc.getPublicKey();
@@ -57,22 +97,47 @@ function testLeasingTx(amount) {
     tra.buildLeasingTx(public_key, recipient, amount, timestamp);
 
     let bytes = tra.toBytes();
+
+    test_lease_id = tra.buildTransactionId(bytes);
     let signature = acc.getSignature(bytes);
-    let sendTx = tra.toJsonForSendingTx(signature);
-    sendLeasingTx(acc, sendTx);
+    let send_tx = tra.toJsonForSendingTx(signature);
 
-    let coldSig = tra.toJsonForColdSignature();
-    console.log("json for cold signature:");
-    console.log(coldSig);
+    let cold_tx = tra.toJsonForColdSignature();
 
-    console.log("test LeasingTx end:");
-}
+    it('get send tx', function () {
+       expect(send_tx).to.not.be.empty;
+    });
+    it('get cold tx', function () {
+        expect(cold_tx).to.not.be.empty;
+    });
+
+    it('get sender publicKey in cold tx', function () {
+        expect(cold_tx['senderPublicKey']).to.be.equal(sender_public_key);
+    });
+    it('get recipient in cold tx', function () {
+        expect(cold_tx['recipient']).to.be.equal(recipient);
+    });
+    it('get correct transactionType in cold tx', function () {
+        expect(cold_tx['transactionType']).to.be.equal(constants.LEASE_TX);
+    });
+    it('get correct opc in cold tx', function () {
+        expect(cold_tx['opc']).to.be.equal(constants.OPC_TRANSACTION);
+    });
+    it('get protocol in cold tx', function () {
+        expect(cold_tx['protocol']).to.be.equal(constants.PROTOCOL);
+    });
+    it('get leasing tx result', async ()=>{
+        let result = await sendLeasingTx(acc, send_tx);
+        expect(result).to.not.be.empty;
+        expect(result['recipient']).to.be.equal(recipient);
+        expect(result['amount']).to.be.equal(send_tx['amount']);
+        expect(result['type']).to.be.equal(constants.LEASE_TX);
+    });
+});
 
 
-//CancelLeasingTx test
-function testCancelLeasingTx() {
-    console.log("test CancelLeasingTx begin:");
-
+//test cancel leasing tx ()
+describe('test cancel leasing tx', function () {
     let acc =  new Account(network_byte);
     acc.buildFromSeed(seed, nonce);
     let public_key = acc.getPublicKey();
@@ -84,33 +149,33 @@ function testCancelLeasingTx() {
 
     let bytes = tra.toBytes();
     let signature = acc.getSignature(bytes);
-    let sendTx = tra.toJsonForSendingTx(signature);
-    sendCancelLeasingTx(acc, sendTx);
-    let coldSig = tra.toJsonForColdSignature();
-    console.log("json for cold signature:");
-    console.log(coldSig);
-    console.log("test CancelLeasingTx end:");
-}
-
-async function sendPaymentTx(acc, tx) {
-    let node = host_ip + '/vsys/broadcast/payment';
-    const result = await acc.sendTransactionTx(node, tx);
-    console.log("result:");
-    console.log(result);
-}
-
-async function sendLeasingTx(acc, tx) {
-    let node = host_ip + '/leasing/broadcast/lease';
-    const result = await acc.sendTransactionTx(node, tx);
-    console.log(result);
-}
-
-async function sendCancelLeasingTx(acc, tx) {
-    let node = host_ip + '/leasing/broadcast/cancel';
-    const result = await acc.sendTransactionTx(node, tx);
-    console.log(result);
-}
-
-testPaymentTx(amount);
-testLeasingTx(amount);
-testCancelLeasingTx();
+    let send_tx = tra.toJsonForSendingTx(signature);
+    let cold_tx = tra.toJsonForColdSignature();
+    it('get send tx', function () {
+        expect(send_tx).to.not.be.empty;
+    });
+    it('get cold tx', function () {
+        expect(cold_tx).to.not.be.empty;
+    });
+    it('get sender publicKey in cold tx', function () {
+        expect(cold_tx['senderPublicKey']).to.be.equal(sender_public_key);
+    });
+    it('get txId in cold tx', function () {
+        expect(cold_tx['txId']).to.be.equal(lease_id);
+    });
+    it('get correct transactionType in cold tx', function () {
+        expect(cold_tx['transactionType']).to.be.equal(constants.CANCEL_LEASE_TX);
+    });
+    it('get correct opc in cold tx', function () {
+        expect(cold_tx['opc']).to.be.equal(constants.OPC_TRANSACTION);
+    });
+    it('get protocol in cold tx', function () {
+        expect(cold_tx['protocol']).to.be.equal(constants.PROTOCOL);
+    });
+    it('get cancel leasing tx result', async ()=>{
+        await sendCancelLeasingTx(acc, send_tx);
+        expect(cancel_lease_result).to.not.be.empty;
+        // expect(cancel_lease_result['id']).to.be.a('string');
+        // expect(cancel_lease_result['type']).to.be.equal(constants.CANCEL_LEASE_TX);
+    });
+});
