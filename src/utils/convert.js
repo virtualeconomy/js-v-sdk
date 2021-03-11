cd1b1bbbe291568ca2312aca1c5c677b70a5e81e
Haynar216 <913636021@qq.com>
2021-03-11 16:55:07 +0800

    parse execution data

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -6,7 +6,11 @@ import bignumber_1 from 'bignumber.js';
 import converters_1 from './converters';
 import Base58 from 'base-58';
 import Crypto from './crypto';
-import Common from './common'
+import Common from './common';
+import * as Constants from '../constants';
+import { TOKEN, SPLITTABLE_TOKEN, LOCK, PAYMENT_CHANNEL, SYSTEM, NFT } from  '../contract_type';
+import { issue, destroy, send, split, abort, collectPayment, createAndLoad, deposit, extendExpirationTime, load, lock, supersede, transfer, unload, withdraw} from "../contract_function_type";
+
 function performBitwiseAnd(a, b) {
     let sa = a.toString(2).split('.')[0];
     let sb = b.toString(2).split('.')[0];
@@ -26,6 +30,52 @@ function parseAmountData(bytes, bytes_length) {
         result += '0'.repeat(len) + bytes[k].toString(2)
     }
     return bytes_length === 8 ? bignumber_1.default(result, 2) : bignumber_1.default(result, 2).toNumber()
+}
+function checkFunctionData(functionType, parsedData) {
+    let length = parsedData.length
+    switch (functionType) {
+        case supersede:
+            if (length !== 1 || parsedData[0]['type'] !== Constants.ACCOUNT_ADDR_TYPE) throw new Error('Invalid functionData')
+            break
+        case issue:
+            if (length !== 1 || !(parsedData[0]['type'] === Constants.SHORTTEXT_TYPE || parsedData[0]['type'] === Constants.AMOUNT_TYPE)) throw new Error('Invalid functionData')
+            break
+        case destroy:case split:
+            if (length !== 1 || parsedData[0]['type'] !== Constants.AMOUNT_TYPE) throw new Error('Invalid functionData')
+            break
+        case send:
+            if (length !== 2 || parsedData[0]['type'] !== Constants.ACCOUNT_ADDR_TYPE || !(parsedData[1]['type'] === Constants.INT32_TYPE || parsedData[1]['type'] === Constants.AMOUNT_TYPE)) throw new Error('Invalid functionData')
+            break
+        case transfer:
+            if (length !== 3 || !(parsedData[0]['type'] === Constants.CONTRACT_ACCOUNT_TYPE || parsedData[0]['type'] === Constants.ACCOUNT_ADDR_TYPE) || !(parsedData[1]['type'] === Constants.CONTRACT_ACCOUNT_TYPE || parsedData[1]['type'] === Constants.ACCOUNT_ADDR_TYPE) || !(parsedData[2]['type'] === Constants.AMOUNT_TYPE || parsedData[2]['type'] === Constants.INT32_TYPE)) throw new Error('Invalid functionData')
+            break
+        case deposit:
+            if (length !== 3 || parsedData[0]['type'] !== Constants.ACCOUNT_ADDR_TYPE || parsedData[0]['type'] !== Constants.CONTRACT_ACCOUNT_TYPE || !(parsedData[2]['type'] === Constants.AMOUNT_TYPE || parsedData[2]['type'] === Constants.INT32_TYPE)) throw new Error('Invalid functionData')
+            break
+        case withdraw:
+            if (length !== 3 || parsedData[0]['type'] !== Constants.CONTRACT_ACCOUNT_TYPE || parsedData[0]['type'] !== Constants.ACCOUNT_ADDR_TYPE || !(parsedData[2]['type'] === Constants.AMOUNT_TYPE || parsedData[2]['type'] === Constants.INT32_TYPE)) throw new Error('Invalid functionData')
+            break
+        case lock:
+            if (length !== 1 || parsedData[0]['type'] !== Constants.TIME_STAMP_TYPE) throw new Error('Invalid functionData')
+            break
+        case createAndLoad:
+            if (length !== 3 || parsedData[0]['type'] !== Constants.ACCOUNT_ADDR_TYPE || parsedData[1]['type'] !== Constants.AMOUNT_TYPE || parsedData[2]['type'] !== Constants.TIME_STAMP_TYPE) throw new Error('Invalid functionData')
+            break
+        case extendExpirationTime:
+            if (length !== 2 || parsedData[0]['type'] !== Constants.SHORT_BYTES_TYPE || parsedData[1]['type'] !== Constants.TIME_STAMP_TYPE) throw new Error('Invalid functionData')
+            break
+        case load:
+            if (length !== 2 || parsedData[0]['type'] !== Constants.SHORT_BYTES_TYPE || parsedData[1]['type'] !== Constants.AMOUNT_TYPE) throw new Error('Invalid functionData')
+            break
+        case abort:case unload:
+            if (length !== 1 || parsedData[0]['type'] !== Constants.SHORT_BYTES_TYPE) throw new Error('Invalid functionData')
+            break
+        case collectPayment:
+            if (length !== 3 || parsedData[0]['type'] !== Constants.SHORT_BYTES_TYPE || parsedData[1]['type'] !== Constants.AMOUNT_TYPE || parsedData[2]['type'] !== Constants.SHORT_BYTES_TYPE) throw new Error('Invalid functionData')
+            break
+        default:
+            throw new Error('Unsupported function type')
+    }
 }
 const Convert = {
     booleanToBytes: function (input) {
@@ -117,6 +167,249 @@ const Convert = {
             }
         }
         return function_data
+    },
+    parseExecutionData(contractType, functionIndex, functionData) {
+        let functionType
+        let returnData = {}
+        let Data = this.parseFunctionData(functionData)
+        switch (contractType) {
+            case TOKEN:
+                switch (functionIndex) {
+                    case Constants.SUPERSEDE_FUNCIDX:
+                        functionType = supersede
+                        checkFunctionData(functionType, Data)
+                        returnData = {'newIssuer': Data[0]['data']}
+                        break
+                    case Constants.ISSUE_FUNCIDX:
+                        functionType = issue
+                        checkFunctionData(functionType, Data)
+                        returnData = {'amount': Data[0]['data']}
+                        break
+                    case Constants.DESTROY_FUNCIDX:
+                        functionType = destroy
+                        checkFunctionData(functionType, Data)
+                        returnData = {'amount': Data[0]['data']}
+                        break
+                    case Constants.SEND_FUNCIDX:
+                        functionType = send
+                        checkFunctionData(functionType, Data)
+                        returnData = {'recipient': Data[0]['data'], 'amount': Data[1]['data']}
+                        break
+                    case Constants.TRANSFER_FUNCIDX:
+                        functionType = transfer
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'sender': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.WITHDRAW_FUNCIDX:
+                        functionType = withdraw
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'contract': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.DEPOSIT_FUNCIDX:
+                        functionType = deposit
+                        checkFunctionData(functionType, Data)
+                        returnData = {'sender': Data[0]['data'], 'contract': Data[1]['data'], 'amount': Data[2]['data']}
+                        break
+                    default:
+                        throw new Error('Wrong functionIndex in ' + TOKEN)
+                }
+                break
+            case SPLITTABLE_TOKEN:
+                switch (functionIndex) {
+                    case Constants.SUPERSEDE_FUNCIDX:
+                        functionType = supersede
+                        checkFunctionData(functionType, Data)
+                        returnData = {'newIssuer': Data[0]['data']}
+                        break
+                    case Constants.ISSUE_FUNCIDX:
+                        functionType = issue
+                        checkFunctionData(functionType, Data)
+                        returnData = {'amount': Data[0]['data']}
+                        break
+                    case Constants.DESTROY_FUNCIDX:
+                        functionType = destroy
+                        checkFunctionData(functionType, Data)
+                        returnData = {'amount': Data[0]['data']}
+                        break
+                    case Constants.SPLIT_FUNCIDX:
+                        functionType = split
+                        checkFunctionData(functionType, Data)
+                        returnData = {'amount': Data[0]['data']}
+                        break
+                    case Constants.SEND_FUNCIDX_SPLIT:
+                        functionType = send
+                        checkFunctionData(functionType, Data)
+                        returnData = {'recipient': Data[0]['data'], 'amount': Data[1]['data']}
+                        break
+                    case Constants.TRANSFER_FUNCIDX_SPLIT:
+                        functionType = transfer
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'sender': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.WITHDRAW_FUNCIDX_SPLIT:
+                        functionType = withdraw
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'contract': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.DEPOSIT_FUNCIDX_SPLIT:
+                        functionType = deposit
+                        checkFunctionData(functionType, Data)
+                        returnData = {'sender': Data[0]['data'], 'contract': Data[1]['data'], 'amount': Data[2]['data']}
+                        break
+                    default:
+                        throw new Error('Wrong functionIndex in ' + SPLITTABLE_TOKEN)
+                }
+                break
+            case SYSTEM:
+                switch (functionIndex) {
+                    case Constants.SYSTEM_CONTRACT_SEND_FUNCIDX:
+                        functionType = send
+                        checkFunctionData(functionType, Data)
+                        returnData = {'recipient': Data[0]['data'], 'amount': Data[1]['data']}
+                        break
+                    case Constants.SYSTEM_CONTRACT_TRANSFER_FUNCIDX:
+                        functionType = transfer
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'sender': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.SYSTEM_CONTRACT_WITHDRAW_FUNCIDX:
+                        functionType = withdraw
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'contract': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.SYSTEM_CONTRACT_DEPOSIT_FUNCIDX:
+                        functionType = deposit
+                        checkFunctionData(functionType, Data)
+                        returnData = {'sender': Data[0]['data'], 'contract': Data[1]['data'], 'amount': Data[2]['data']}
+                        break
+                    default:
+                        throw new Error('Wrong functionIndex in ' + SYSTEM)
+                }
+                break
+            case LOCK:
+                if (functionIndex === Constants.LOCK_CONTRACT_LOCK_FUNCIDX) {
+                    functionType = lock
+                    checkFunctionData(functionType, Data)
+                    returnData = {'timestamp': Data[0]['data']}
+                } else throw new Error('Wrong functionIndex in ' + LOCK)
+                break
+            case PAYMENT_CHANNEL:
+                switch (functionIndex) {
+                    case Constants.PAYMENTCHANNEL_CONTRACT_CREATEANDLOAD_FUNCIDX:
+                        functionType = createAndLoad
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'recipient': Data[0]['data'],
+                            'amount': Data[1]['data'],
+                            'expirationTime': Data[2]['data']
+                        }
+                        break
+                    case Constants.PAYMENTCHANNEL_CONTRACT_EXTENDEXPIRATIONTIME_FUNCIDX:
+                        functionType = extendExpirationTime
+                        checkFunctionData(functionType, Data)
+                        returnData = {'channelId': Data[0]['data'], 'expirationTime': Data[1]['data']}
+                        break
+                    case Constants.PAYMENTCHANNEL_CONTRACT_LOAD_FUNCIDX:
+                        functionType = load
+                        checkFunctionData(functionType, Data)
+                        returnData = {'channelId': Data[0]['data'], 'amount': Data[1]['data']}
+                        break
+                    case Constants.PAYMENTCHANNEL_CONTRACT_ABORT_FUNCIDX:
+                        functionType = abort
+                        checkFunctionData(functionType, Data)
+                        returnData = {'channelId': Data[0]['data']}
+                        break
+                    case Constants.PAYMENTCHANNEL_CONTRACT_UNLOAD_FUNCIDX:
+                        functionType = unload
+                        checkFunctionData(functionType, Data)
+                        returnData = {'channelId': Data[0]['data']}
+                        break
+                    case Constants.PAYMENTCHANNEL_CONTRACT_COLLECTPAYMENT_FUNCIDX:
+                        functionType = collectPayment
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'channelId': Data[0]['data'],
+                            'amount': Data[1]['data'],
+                            'signature': Data[2]['data']
+                        }
+                        break
+                    default:
+                        throw new Error('Wrong functionIndex in ' + PAYMENT_CHANNEL)
+
+                }
+                break
+            case NFT:
+                switch (functionIndex) {
+                    case Constants.NFT_CONTRACT_SUPERSEDE_FUNCIDX:
+                        functionType = supersede
+                        checkFunctionData(functionType, Data)
+                        returnData = {'newIssuer': Data[0]['data']}
+                        break
+                    case Constants.NFT_CONTRACT_ISSUE_FUNCIDX:
+                        functionType = issue
+                        checkFunctionData(functionType, Data)
+                        returnData = {'tokenDescription': Data[0]['data']}
+                        break
+                    case Constants.NFT_CONTRACT_SEND_FUNCIDX:
+                        functionType = send
+                        checkFunctionData(functionType, Data)
+                        returnData = {'recipient': Data[0]['data'], 'tokenIndex': Data[1]['data']}
+                        break
+                    case Constants.NFT_CONTRACT_TRANSFER_FUNCIDX:
+                        functionType = transfer
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'sender': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'tokenIndex': Data[2]['data']
+                        }
+                        break
+                    case Constants.NFT_CONTRACT_WITHDRAW_FUNCIDX:
+                        functionType = withdraw
+                        checkFunctionData(functionType, Data)
+                        returnData = {
+                            'contract': Data[0]['data'],
+                            'recipient': Data[1]['data'],
+                            'amount': Data[2]['data']
+                        }
+                        break
+                    case Constants.NFT_CONTRACT_DEPOSIT_FUNCIDX:
+                        functionType = deposit
+                        checkFunctionData(functionType, Data)
+                        returnData = {'sender': Data[0]['data'], 'contract': Data[1]['data'], 'amount': Data[2]['data']}
+                        break
+                    default:
+                        throw new Error('Wrong functionIndex in ' + NFT)
+                }
+                break;
+            default:
+                throw new Error('Unsupported contract type')
+        }
+        return { functionType: functionType, functionData: returnData }
     },
     stringToByteArray: function (input) {
         if (typeof input !== 'string') {
