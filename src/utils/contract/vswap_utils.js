eb9ac6ef0493e991ca7281d7413f3e91c82b2d6f
twwu123 <tszwai@v.systems>
2021-07-16 13:58:33 +0800

    update vswap util variable name

--- src/utils/contract/vswap_utils.js
+++ src/utils/contract/vswap_utils.js
@@ -1,36 +1,36 @@
 import BigNumber from "bignumber.js";
 
-export function calculateSwapTokenForExactBaseToken(amountOut, reservedA, reservedB) {
-  let mulValue = new BigNumber(reservedB).multipliedBy(1000);
+export function calculateSwapTokenForExactBaseToken(amountOut, reservedBase, reservedTarget) {
+  let mulValue = new BigNumber(reservedTarget).multipliedBy(1000);
   let numerator = mulValue.multipliedBy(amountOut);
-  let subValue = new BigNumber(reservedA).minus(amountOut);
+  let subValue = new BigNumber(reservedBase).minus(amountOut);
   let denominator = subValue.multipliedBy(997);
   let result = numerator.dividedBy(denominator)
-  return result.integerValue(BigNumber.ROUND_DOWN) + 1;
+  return result.integerValue(BigNumber.ROUND_DOWN).plus(1);
 }
 
-export function calculateSwapExactTokenForBaseToken(amountIn, reservedA, reservedB) {
+export function calculateSwapExactTokenForBaseToken(amountIn, reservedBase, reservedTarget) {
   let amountInWithoutFee = new BigNumber(amountIn).multipliedBy(997);
-  let mulValue = new BigNumber(reservedB).multipliedBy(1000);
-  let numerator = new BigNumber(reservedA).multipliedBy(amountInWithoutFee);
+  let mulValue = new BigNumber(reservedTarget).multipliedBy(1000);
+  let numerator = new BigNumber(reservedBase).multipliedBy(amountInWithoutFee);
   let denominator = mulValue.plus(amountInWithoutFee);
   let result = numerator.dividedBy(denominator);
   return result.integerValue(BigNumber.ROUND_DOWN);
 }
 
-export function calculateSwapTokenForExactTargetToken(amountOut, reservedA, reservedB) {
-  let mulValue = new BigNumber(reservedA).multipliedBy(1000);
+export function calculateSwapTokenForExactTargetToken(amountOut, reservedBase, reservedTarget) {
+  let mulValue = new BigNumber(reservedBase).multipliedBy(1000);
   let numerator = mulValue.multipliedBy(amountOut);
-  let subValue = new BigNumber(reservedB).minus(amountOut);
+  let subValue = new BigNumber(reservedTarget).minus(amountOut);
   let denominator = subValue.multipliedBy(997);
   let result = numerator.dividedBy(denominator);
-  return result.integerValue(BigNumber.ROUND_DOWN) + 1;
+  return result.integerValue(BigNumber.ROUND_DOWN).plus(1);
 }
 
-export function calculateSwapExactTokenForTargetToken(amountIn, reservedA, reservedB) {
+export function calculateSwapExactTokenForTargetToken(amountIn, reservedBase, reservedTarget) {
   let amountInWithoutFee = new BigNumber(amountIn).multipliedBy(997);
-  let mulValue = new BigNumber(reservedA).multipliedBy(1000);
-  let numerator = new BigNumber(reservedB).multipliedBy(amountInWithoutFee);
+  let mulValue = new BigNumber(reservedBase).multipliedBy(1000);
+  let numerator = new BigNumber(reservedTarget).multipliedBy(amountInWithoutFee);
   let denominator = mulValue.plus(amountInWithoutFee);
   let result = numerator.dividedBy(denominator)
   return result.integerValue(BigNumber.ROUND_DOWN);
